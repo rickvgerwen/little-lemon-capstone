20748aea0ccd8f7e10d84ae3ea478ed6906479f3
rickvgerwen <rick.van.gerwen@sogeti.com>
2023-05-16 11:38:04 +0200

    Added mobile navigation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,13 +6,15 @@ const Hero = () => {
 	return (
 		<section className="hero">
 			<div className="container">
-				<h1>Little Lemon</h1>
-				<span className="sub-title">Chicago</span>
-				<p className="lead-text">
-					We are a family owned Mediterranean restaurant, focused on traditional
-					recipes served with a modern twist.
-				</p>
-				<Button text="Reserve a table" link="/booking" />
+				<div className="left">
+					<h1>Little Lemon</h1>
+					<span className="sub-title">Chicago</span>
+					<p className="lead-text">
+						We are a family owned Mediterranean restaurant, focused on
+						traditional recipes served with a modern twist.
+					</p>
+					<Button text="Reserve a table" link="/booking" />
+				</div>
 				<div className="img-container">
 					<img src={heroImg} alt="Tapas" className="hero-img" />
 				</div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,36 +3,44 @@ import "../styles/NavBar.css";
 
 const NavBar = () => {
 	return (
-		<nav>
-			<ul className="nav-menu">
-				<li>
-					<NavLink to="/">Home</NavLink>
-				</li>
-				<li>
-					<a href="#" className="disabled-nav-link">
-						About
-					</a>
-				</li>
-				<li>
-					<a href="#" className="disabled-nav-link">
-						Menu
-					</a>
-				</li>
-				<li>
-					<NavLink to="/booking">Reservations</NavLink>
-				</li>
-				<li>
-					<a href="#" className="disabled-nav-link">
-						Order Online
-					</a>
-				</li>
-				<li>
-					<a href="#" className="disabled-nav-link">
-						Login
-					</a>
-				</li>
-			</ul>
-		</nav>
+		<>
+			<label htmlFor="toggle-mobile-nav" className="toggle-menu">
+				<span></span>
+				<span></span>
+				<span></span>
+			</label>
+			<input type="checkbox" id="toggle-mobile-nav"></input>
+			<nav>
+				<ul className="nav-menu">
+					<li>
+						<NavLink to="/">Home</NavLink>
+					</li>
+					<li>
+						<a href="/" className="disabled-nav-link">
+							About
+						</a>
+					</li>
+					<li>
+						<a href="/" className="disabled-nav-link">
+							Menu
+						</a>
+					</li>
+					<li>
+						<NavLink to="/booking">Reservations</NavLink>
+					</li>
+					<li>
+						<a href="/" className="disabled-nav-link">
+							Order Online
+						</a>
+					</li>
+					<li>
+						<a href="/" className="disabled-nav-link">
+							Login
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</>
 	);
 };
 
